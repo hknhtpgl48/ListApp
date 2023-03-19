0f706139b544374b1d81d0e4703e5738d5c52cfa
hknhtpgl48 <hkn.hatipolu48@gmail.com>
2023-03-19 20:31:18 +0300

    adding&deleting items using UIAlertController

--- ListApp/Controllers/ViewController.swift
+++ ListApp/Controllers/ViewController.swift
@@ -8,7 +8,8 @@
 import UIKit
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-    let data = ["Swift", "Kotlin", "C++", "C", "Python", "Java", "C#"]
+    var data = [String]()
+    var alertController = UIAlertController()
     // table View'ı tanımlayalım
     @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
@@ -28,7 +29,74 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         cell.textLabel?.text = data[indexPath.row]
         return cell
     }
-
-
+    
+    @IBAction func addBarButtonPressed(_ sender: UIBarButtonItem) {
+        presentAddAlert()
+    }
+    
+    @IBAction func removeBarButtonPressed(_ sender: UIBarButtonItem) {
+        presentAlert(title: "Uyarı!",
+                     message: "Listedeki bütün ögeleri silmek istediğinize emin misiniz?",
+                     defaultButtonTitle: "Evet",
+                     cancelButtonTitle: "Vazgeç") { _ in
+            self.data.removeAll()
+            self.tableView.reloadData()
+        }
+    }
+    
+    func presentAddAlert() {
+        presentAlert(title: "Yeni Eleman Ekle",
+                     message: nil,
+                     defaultButtonTitle: "Ekle",
+                     cancelButtonTitle: "Vazgeç",
+                     isTextFieldAvaliable: true,
+                     defaultButtonHandler: { _ in
+                            let text = self.alertController.textFields?.first?.text
+                            if text != "" {
+                                self.data.append((text)!)
+                                self.tableView.reloadData()
+                            } else {
+                                self.presentWarningAlert()
+                            }
+                        })
+        
+    }
+    
+    func presentWarningAlert() {
+        presentAlert(title: "Uyarı!",
+                     message: "Liste elemanı boş olamaz." ,
+                     cancelButtonTitle: "Tamam")
+    }
+    
+    func presentAlert(title: String?,
+                      message: String?,
+                      preferredStyle: UIAlertController.Style = .alert,
+                      defaultButtonTitle: String? = nil,
+                      cancelButtonTitle: String?,
+                      isTextFieldAvaliable: Bool = false,
+                      defaultButtonHandler: ((UIAlertAction) -> Void)? = nil) {
+        //alert oluşturma
+        alertController = UIAlertController(title: title,
+                                                message: message,
+                                                preferredStyle: preferredStyle)
+        //default button
+        if defaultButtonTitle != nil {
+            let defaultButton = UIAlertAction(title: defaultButtonTitle,
+                                              style: .default,
+                                              handler: defaultButtonHandler)
+            alertController.addAction(defaultButton)
+        }
+        //cancel button
+        let cancelButton = UIAlertAction(title: cancelButtonTitle,
+                                         style: .cancel)
+        
+        if isTextFieldAvaliable {
+            alertController.addTextField()
+        }
+        
+        alertController.addAction(cancelButton)
+        self.present(alertController, animated: true)
+    }
+    
 }
 
