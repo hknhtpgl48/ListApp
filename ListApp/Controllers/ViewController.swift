3c4912865f1eb1f642c62013f8031511fa807e55
hknhtpgl48 <hkn.hatipolu48@gmail.com>
2023-03-19 12:33:06 +0300

    listing some data with UITableView

--- ListApp/Controllers/ViewController.swift
+++ ListApp/Controllers/ViewController.swift
@@ -7,11 +7,26 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    let data = ["Swift", "Kotlin", "C++", "C", "Python", "Java", "C#"]
+    // table View'ı tanımlayalım
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count// aşağıdaki methodun kaç kere çalışacağını da belirtiyor
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //var cell = UITableViewCell()
+        //Reussable cell oluşturma
+        let cell = tableView.dequeueReusableCell(withIdentifier: "defaultCell", for: indexPath)
+        cell.textLabel?.text = data[indexPath.row]
+        return cell
     }
 
 
